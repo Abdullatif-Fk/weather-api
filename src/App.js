a247bbb396fbd1c227103fef1ad7ce0fb7da7223
Abdullatif-Fk <abdul647.fk@gmail.com>
2020-11-02 01:02:06 +0200

    Added my name to the app!

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Karim"
+      name: "Abdullatif"
     };
   }
 
@@ -20,10 +20,63 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+      <div id="app">
+        <div className="search">
+          <Search handleInput={this.handleInputChange} />
+        </div>
+        <div className="content">
+          
+          <img src="./clear.svg" alt="cloudy" width="150px" height="150px"></img>
+          <p>overcast clouds</p>
+          
+         <div className="temperature-today">
+         <span> Temperature </span> 10&#8451; to 11&#8451;
+         
+         </div>
+         
+         
+         <div className="info">
+            <p>Humidity</p>
+            <p>78%</p>
+            <p>Pressure</p>
+            <p>1008.48</p>
+         </div>
+            
+          
+          <div className="details">
+            <div className="times">
+              
+                <p>03:00</p>
+                <p>06:00</p>
+                <p>09:00</p>
+                <p>12:00</p>
+                <p>15:00</p>
+                <p>18:00</p>
+                <p>21:00</p>
+              
+            </div>
+             <div className="images">
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+                <img src="./clear.svg" alt="clear" width="50px" height="50px"></img>
+            </div>
+            <div className="temperatures">
+              
+                <p>8&#8451;</p>
+                <p>9&#8451;</p>
+                <p>14&#8451;</p>
+                <p>17&#8451;</p>
+                <p>18&#8451;</p>
+                <p>16&#8451;</p>
+                <p>13&#8451;</p>
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
+  //const div = document.createElement("div");
+  const div = document.getElementById("app");
+
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
